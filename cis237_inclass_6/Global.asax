54ae625ce1c8a7a5422c3b5870d136b37e986904
David Barnes <dbarnes@kvcc.edu>
2018-11-27 15:06:25 -0500

    Initial Commit.

---
+++ cis237_inclass_6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cis237_inclass_6.MvcApplication" Language="C#" %>
---
+++ cis237_inclass_6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cis237_inclass_6.Startup))]
+namespace cis237_inclass_6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
