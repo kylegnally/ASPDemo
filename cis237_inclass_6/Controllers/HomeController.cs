83efe8196fa8676835d7dfd7fed56b4b334eca0b
David Barnes <dbarnes@kvcc.edu>
2018-12-04 17:29:48 -0500

    Add the car model. Add car controller and views from scaffolding. Add nav links to layout. Protect car links and methods from unauthenticated users.

--- cis237_inclass_6/Controllers/HomeController.cs
+++ cis237_inclass_6/Controllers/HomeController.cs
@@ -27,9 +27,28 @@ namespace cis237_inclass_6.Controllers
 
         public ActionResult Contact()
         {
+            // This is the ViewBag. It can be used to store values that we want
+            // to send over to the view from the controller.
+            // ViewBag is a dynamic object where we can just add properties on
+            // the fly to it, and then access them in the view.
             ViewBag.Message = "Your contact page.";
 
+            // View is one more ActionResult that will look into the Views folder
+            // for a view that matches the controller and method.
+            // Inside the Views folder it will look for a Home folder. Then it
+            // will look for a Contact.cshtml page inside that folder.
             return View();
+        }
+
+        public ActionResult Foobar()
+        {
+            // Content is one of the ActionResult return types that can be
+            // returned. Content just returns a string, but is good for testing
+            // to ensure that a url returns the correct controller method.
+            // Once that is established, you may want to change it to on of the other
+            // types.
+            return Content("The foobar page");
+
         }
     }
 }
